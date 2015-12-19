a495cd33645404100c892b6bdb1e28d63263b1ad
Tommy <tommy.jin@curiositychina.com>
2015-12-19 16:47:59 +0800

    add skrollr

--- src/app/components/List.jsx
+++ src/app/components/List.jsx
@@ -9,22 +9,57 @@ const List = React.createClass({
 	  };
 	},
 
-    componentDidMount() {
-      	window.addEventListener( 'scroll', this.scrollImage );
-    },
+    // componentDidMount() {
+    //   	window.addEventListener( 'scroll', this.scrollImage );
+    // },
 
-    scrollImage() {
-    	window.requestAnimationFrame(this.updatePosition);
+    // scrollImage() {
+    // 	window.requestAnimationFrame(this.updatePosition);
+    // },
+
+    componentDidMount() {
+    	//window.requestAnimationFrame(this.updatePosition);
     },
 
     updatePosition() {
-    	this.state.imgX = window.pageYOffset || window.document.documentElement.scrollTop;
+    	//this.state.imgX = window.pageYOffset || document.documentElement.scrollTop;
+    	//let moveY = window.pageYOffset;
+
     	let img = ReactDOM.findDOMNode(this.refs.image);
-    	let amount = this.state.imgX/-5;
+
+    	//let moveY = window.skrollr.getScrollTop(img);
+
+    	let moveY = img.getBoundingClientRect().top;
+
+    	let amount = moveY/-10;
     	img.style.transform = "translateY(" + amount + "px)";
 
     	window.requestAnimationFrame(this.updatePosition);
     },
+
+    // componentDidMount() {
+    // 	window.addEventListener('touchstart', this.touchStart);
+    //   	window.addEventListener( 'touchmove', this.handleMove );
+    // },
+
+    // touchStart(evt) {
+    // 	evt.preventDefault();
+    // 	let touchobj = ev.changedTouches[0];
+    // 	this.state.imgX = parseInt(touchobj.pageY);
+    // },
+
+    // handleMove(evt) {
+    // 	let touches = evt.changedTouches;
+    // 	let img = ReactDOM.findDOMNode(this.refs.image);
+    // 	let moveY = 0;
+
+    // 	moveY = this.state.imgX - touches[0].pageY;
+
+    // 	console.log(moveY);
+
+    // 	let amount = moveY/10;
+    // 	img.style.transform = "translateY(" + amount + "px)";
+    // },
 
     render() {
       // let rollY = this.state.imgX/-5;
@@ -33,18 +68,17 @@ const List = React.createClass({
       // };
       return (
       	  <div className="li-main">
-          <section className="li-header">About</section>
-          <Box center vertical className="li-body">
-          		<div className="li-box"></div>
-          		<div className="li-box"></div>
+          <Box id="skrollr-body" center vertical className="li-body">
           		<div className="li-box"></div>
           		<div className="li-box">
           			<img ref="image" className="li-box-img" src={'./images/green-goddess-sandwiches-31.jpg'} />
           		</div>
-          		<div className="li-box"></div>
           		<div className="li-box">
-          			<img className="li-box-img" src={'./images/green-goddess-sandwiches-41.jpg'} />
-          		</div>
+                    <img className="li-box-img" src={'./images/green-goddess-sandwiches-31.jpg'} data-bottom-top="transform: translate3d(0px, -30%, 0px);" data-top-bottom="transform: translate3d(0px, 0%, 0px);" />
+                </div>
+          		<div className="li-box"></div>
+          		<div className="li-box"></div>
+          		<div className="li-box"></div>
           		<div className="li-box"></div>
           		<div className="li-box"></div>
           		<div className="li-box"></div>
